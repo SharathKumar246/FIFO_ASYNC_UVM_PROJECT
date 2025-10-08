d9525dca36640d6f0ef1f5fbbbdc9cc3053e7488
Azure-Draco <skumarupadhya6@gmail.com>
2025-10-08 14:38:05 +0530

    ..

--- src/fifo_if.sv
+++ src/fifo_if.sv
@@ -5,13 +5,13 @@ interface fifo_if  (input bit wclk, input bit rclk, input bit wrst_n, input bit 
   logic [`DATA_WIDTH-1:0] wdata;
   logic winc;
   bit wfull;
-  logic [`ADDR_WIDTH:0] wptr;
+ 
   
   // Read interface signals
   logic [`DATA_WIDTH-1:0] rdata;
   logic rinc;
   logic rempty;
-  logic [`ADDR_WIDTH:0] rptr;
+ 
 
   // Clocking blocks for synchronization
   clocking wdrv_cb @(posedge wclk);
@@ -19,14 +19,14 @@ interface fifo_if  (input bit wclk, input bit rclk, input bit wrst_n, input bit 
     output wdata;
     output winc;
     input wfull;
-    // input wptr;
+  
   endclocking
   
   clocking rdrv_cb @(posedge rclk);
     // default input #0 output #0;
     output rinc;
     input rempty;
-    // input rptr;
+  
   endclocking
   
   // Monitor clocking blocks
@@ -35,7 +35,7 @@ interface fifo_if  (input bit wclk, input bit rclk, input bit wrst_n, input bit 
     input wdata;
     input winc;
     input wfull;
-    input wptr;
+
   endclocking
   
   clocking rmon_cb @(posedge rclk);
@@ -43,7 +43,7 @@ interface fifo_if  (input bit wclk, input bit rclk, input bit wrst_n, input bit 
     input rdata;
     input rinc;
     input rempty;
-    input rptr;
+
   endclocking
   
   // // Modports for drivers
